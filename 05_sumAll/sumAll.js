036dd518b0f4fd33d3b0209c7e280b363a384e45
Braden Vail <bradenvail30@gmail.com>
2024-09-23 17:21:36 -0400

    Added solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  const max = Math.max(a, b);
+  const min = Math.min(a, b);
 
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  } else {
+    return (max * (max + 1)) / 2 - ((min - 1) * min) / 2;
+  }
 };
 
 // Do not edit below this line
