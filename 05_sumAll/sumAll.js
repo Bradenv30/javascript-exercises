4bdad77e08f2f79dd47b2ecc79b71033f7b632b7
Braden Vail <bradenvail30@gmail.com>
2024-09-23 17:24:57 -0400

    Added comment that explains sum code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,6 +5,7 @@ const sumAll = function (a, b) {
   if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
     return "ERROR";
   } else {
+    //Formula for the sum of n, reference wikipedia 1+2+3+4...
     return (max * (max + 1)) / 2 - ((min - 1) * min) / 2;
   }
 };
