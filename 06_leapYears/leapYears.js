4bda56c693d8415eadad0de8dd7c24e98275b8d4
Braden Vail <bradenvail30@gmail.com>
2024-09-23 17:40:42 -0400

    Added solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  //if year meets all the requirments, return true. If not, return false
+  //if year is divisible by 4, and both 100 and 400, return true
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
+//If a leap year, return true, if not return false
 // Do not edit below this line
 module.exports = leapYears;
